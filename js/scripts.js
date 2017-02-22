f6018c1e19f503c2307fb1ccfe4729117d8b9928
abuckenberger <abuckenberger@eagles.ewu.edu>
2017-02-22 13:44:40 -0800

    Randomize Img
    *random homepage img
    *js to randomize

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
-// YOUR SCRIPTS GO HERE
+// STORE 3 IMAGES
+
+var img0 = '../img/200.jpg';
+var img1 = '../img/300.jpg';
+var img2 = '../img/400.jpg';
+
+
+
+// GET RANDOM NUMBER
+
+var max = 3;
+var numRand = Math.floor( Math.random() * max );
+console.log(numRand);
+
+
+// DISPLAY RANDOMIZED IMAGES
+
+$('img').attr('src', eval('img' + numRand));
+
